ccdf0dacea8177171d5cc8fdaf4c3de9b37a6cf7
Nick Kocharhook <nick@kocharhook.com>
2024-09-29 23:39:38 -0400

    Initial Commit

---
+++ NurfTests/NurfTests.swift
@@ -0,0 +1,17 @@
+//
+//  NurfTests.swift
+//  NurfTests
+//
+//  Created by Nick Kocharhook on 2024-09-29.
+//
+
+import Testing
+@testable import Nurf
+
+struct NurfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
